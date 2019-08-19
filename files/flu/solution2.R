f55eb68d23f67ca0e82175c945eb5b09e8a4c1c6
arranhamlet <arran.hamlet14@imperial.ac.uk>
2019-08-19 17:17:57 +0100

    Updating flu practicals

--- files/flu/solution2.R
+++ files/flu/solution2.R
@@ -1,23 +1,21 @@
 # initial conditions
-initial(S) <- N - E_0
-initial(E) <- E_0
-initial(I) <- 0
+initial(S) <- N - I_0
+initial(E) <- 0
+initial(I) <- I_0
 initial(R) <- 0
-initial(J) <- 0
 
 # equations
 deriv(S) <- -beta * S * I / N
 deriv(E) <- beta * S * I / N - gamma * E
 deriv(I) <- gamma * E - sigma * I
 deriv(R) <- sigma * I
-deriv(J) <- gamma * E
 
 # parameter values
-R_0 <- user()
+R_0 <- user(1.5)
 R_0_closure <- user()
-L <- user()
-D <- user()
-E_0 <- user(1) # default value
+L <- user(1)
+D <- user(1)
+E_0 <- 1 
 N <- user(370)
 
 # convert parameters
@@ -25,4 +23,6 @@ gamma <- 1 / L
 sigma <- 1 / D
 beta <- if (t > 18 && t < 25) R_0_closure * sigma else R_0 * sigma
 
+#Output
+output(onset) <- if(t == 0) E_0 else gamma*E
 
