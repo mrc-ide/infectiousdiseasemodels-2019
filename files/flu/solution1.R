90ae020b2df729dc13d809d6bc4e1a53c22ae525
arranhamlet <arran.hamlet14@imperial.ac.uk>
2019-08-21 11:16:38 +0100

    Further Patrick refinements

--- files/flu/solution1.R
+++ files/flu/solution1.R
@@ -12,17 +12,15 @@ deriv(R) <- sigma * I
 
 # parameter values
 R_0 <- user(1.5)
-R_0_closure <- user()
 L <- user(1)
 D <- user(1)
-E_0 <- 1 
-N <- user(370)
+I_0 <- 1 # default value
+N <- 370
 
 # convert parameters
 gamma <- 1 / L
 sigma <- 1 / D
-beta <- if (t > 18 && t < 25) R_0_closure * sigma else R_0 * sigma
+beta <- R_0 * sigma
 
 #Output
-output(onset) <- if(t == 0) E_0 else gamma*E
-
+output(onset) <- if(t == 0) I_0 else gamma*E
--- files/flu/solution2.R
+++ files/flu/solution2.R
@@ -12,15 +12,17 @@ deriv(R) <- sigma * I
 
 # parameter values
 R_0 <- user(1.5)
+R_0_closure <- user(1.5)
 L <- user(1)
 D <- user(1)
-I_0 <- 1 # default value
+I_0 <- 1 
 N <- 370
 
 # convert parameters
 gamma <- 1 / L
 sigma <- 1 / D
-beta <- R_0 * sigma
+beta <- if (t > 18 && t < 25) R_0_closure * sigma else R_0 * sigma
 
 #Output
 output(onset) <- if(t == 0) I_0 else gamma*E
+
